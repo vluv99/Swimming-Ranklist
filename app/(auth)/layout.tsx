343e2665697e742e57d4db3c047520d4cdfb3de9
vluv99 <v.luva99@gmail.com>
2024-12-02 22:06:47 +0100

    auth pages updated dark theme

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,7 +10,9 @@ function AuthLayout({ children }: Props): ReactNode {
     <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
       <div className="h-full lg:flex flex-col items-center justify-center px-4">
         <div className="text-center space-y-4 pt-16">
-          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome back!</h1>
+          <h1 className="font-bold text-3xl text-[#2E2A47] dark:text-white">
+            Welcome back!
+          </h1>
           <p className="text-base text-[#7E8CA0] w-3/4 justify-self-center">
             Log in or Create an account to check out the Hungarian Swimming
             Records between 2000 - 2024.
@@ -20,7 +22,7 @@ function AuthLayout({ children }: Props): ReactNode {
           </div>
         </div>
       </div>
-      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center dark:bg-slate-700">
         <Image
           src="/swimming.svg"
           alt="logo"
