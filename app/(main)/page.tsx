325037376cf515da113539330c09cde097147564
vluv99 <v.luva99@gmail.com>
2024-12-03 01:37:02 +0100

    initial DataTable component

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,24 @@
+import { DataTable } from "@/components/data-table";
+import { columns, Payment } from "@/app/(main)/columns";
+import { Card, CardContent } from "@/components/ui/card";
+
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+];
+
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardContent>
+          <DataTable columns={columns} data={data} />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ import { ThemedUserButton } from "@/components/clerk-themed-components";
 
 export function Header() {
   return (
-    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36 dark:from-slate-700 dark:to-slate-800">
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36 dark:from-slate-700 dark:to-slate-900">
       <div className="max-w-screen-2xl mx-auto">
         <div className="w-full flex items-center justify-between mb-14">
           <div className="flex items-center lg:gap-x-16">
